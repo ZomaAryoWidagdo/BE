03207d2b5916fcc373e38a95ceb46393a1cbc1b4
Zoma Aryo Widagdo <zozozzzoo2@rocketmail.com>
2022-07-01 15:40:22 +0700

    make it better

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -1,23 +1,28 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://bkusers:user@cluster0.otdyf.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://bkusers:user@cluster0.otdyf.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 let db;
 async function connection() {
-    try {
-        await client.connect();
-        db = client.db("wishlist");
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await client.connect();
+    db = client.db("wishlist");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 function getDb() {
-    return db;
+  return db;
 }
 
 module.exports = {
-    connection,
-    getDb,
-    client,
+  connection,
+  getDb,
+  client,
 };
